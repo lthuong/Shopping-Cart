b97eebfd42dc9ca6dd6a680d5cd190d87e4b9e25
jdrandomperson <33594579+lthuong@users.noreply.github.com>
2023-03-25 20:02:03 +0100

    Add login functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from "./app-routing.module";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
-import { NavComponent } from './components/shared/nav/nav.component';
-import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
-import { FiltersComponent } from './components/shopping-cart/filters/filters.component';
-import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
-import { CartComponent } from './components/shopping-cart/cart/cart.component';
-import { CartItemComponent } from './components/shopping-cart/cart/cart-item/cart-item.component';
-import { ProductItemComponent } from './components/shopping-cart/product-list/product-item/product-item.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/shared/header/header.component";
+import { FooterComponent } from "./components/shared/footer/footer.component";
+import { NavComponent } from "./components/shared/nav/nav.component";
+import { ShoppingCartComponent } from "./components/shopping-cart/shopping-cart.component";
+import { FiltersComponent } from "./components/shopping-cart/filters/filters.component";
+import { ProductListComponent } from "./components/shopping-cart/product-list/product-list.component";
+import { CartComponent } from "./components/shopping-cart/cart/cart.component";
+import { CartItemComponent } from "./components/shopping-cart/cart/cart-item/cart-item.component";
+import { ProductItemComponent } from "./components/shopping-cart/product-list/product-item/product-item.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { PageNotFoundComponent } from "./components/shared/page-not-found/page-not-found.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -32,14 +33,10 @@ import { PageNotFoundComponent } from './components/shared/page-not-found/page-n
     ProductItemComponent,
     LoginComponent,
     RegisterComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  model = { username: "", password: "", remember: false };
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login(form: NgForm) {
+    // submit the data
   }
-
 }
